2c336e6b4000e4301216a310169a41c8a6dc4896
Gigi <dergigi@pm.me>
2025-04-22 13:11:53 +0100

    feat(types): add never return type to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
-export default function Home() {
+export default function Home(): never {
   // Redirect to the default npub
   redirect('/npub1n00yy9y3704drtpph5wszen64w287nquftkcwcjv7gnnkpk2q54s73000n')
 } 
